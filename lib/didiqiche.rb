acb69c7116d4bf9b0e8fe0d8cc2288a70f7ddd34
Zhu Ran <zhuran94@163.com>
2016-08-05 22:08:16 +0800

    完成基本主页

---
+++ lib/didiqiche.rb
@@ -0,0 +1,55 @@
+require 'hanami/model'
+require 'hanami/mailer'
+Dir["#{ __dir__ }/didiqiche/**/*.rb"].each { |file| require_relative file }
+
+Hanami::Model.configure do
+  ##
+  # Database adapter
+  #
+  # Available options:
+  #
+  #  * File System adapter
+  #    adapter type: :file_system, uri: 'file:///db/bookshelf_development'
+  #
+  #  * Memory adapter
+  #    adapter type: :memory, uri: 'memory://localhost/didiqiche_development'
+  #
+  #  * SQL adapter
+  #    adapter type: :sql, uri: 'sqlite://db/didiqiche_development.sqlite3'
+  #    adapter type: :sql, uri: 'postgres://localhost/didiqiche_development'
+  #    adapter type: :sql, uri: 'mysql://localhost/didiqiche_development'
+  #
+  adapter type: :sql, uri: ENV['DATABASE_URL']
+
+  ##
+  # Migrations
+  #
+  migrations 'db/migrations'
+  schema     'db/schema.sql'
+
+  ##
+  # Database mapping
+  #
+  # Intended for specifying application wide mappings.
+  #
+  mapping do
+    # collection :users do
+    #   entity     User
+    #   repository UserRepository
+    #
+    #   attribute :id,   Integer
+    #   attribute :name, String
+    # end
+  end
+end.load!
+
+Hanami::Mailer.configure do
+  root "#{ __dir__ }/didiqiche/mailers"
+
+  # See http://hanamirb.org/guides/mailers/delivery
+  delivery do
+    development :test
+    test        :test
+    # production :smtp, address: ENV['SMTP_PORT'], port: 1025
+  end
+end.load!
